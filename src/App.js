4f3d43e39dd993bd4f10130c411b9279ff4df15c
Karthik04 <keshwarkarthi914@gmail.com>
2025-11-20 10:58:58 +0530

    log removed

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ function AppWrapper() {
 function App() {
   const [role, setRole] = useState("");
   const { showAlert } = useAlert();
-  console.log("check");
-
   // Override default alert
   useEffect(() => {
     window.alert = (message) => {
